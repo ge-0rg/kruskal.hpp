b6d5dc95725a37e3e19630d5b21645f23f32173c
ireanarch <ireanarch@example.com>
2026-06-02 18:38:34 +0300

    Fix code style

--- methods/kuhn_method.cpp
+++ methods/kuhn_method.cpp
@@ -1,4 +1,5 @@
 #include "kuhn.hpp"
+
 #include <vector>
 
 namespace graph {
@@ -7,29 +8,29 @@ bool Dfs(int v,
          const std::vector<std::vector<int>>& g,
          std::vector<int>& mt,
          std::vector<bool>& used) {
-    if (used[v]) return false;
-    used[v] = true;
+  if (used[v]) return false;
+  used[v] = true;
 
-    for (size_t i = 0; i < g[v].size(); ++i) {
-        int to = g[v][i];
-        if (mt[to] == -1 || Dfs(mt[to], g, mt, used)) {
-            mt[to] = v;
-            return true;
-        }
+  for (size_t i = 0; i < g[v].size(); ++i) {
+    int to = g[v][i];
+    if (mt[to] == -1 || Dfs(mt[to], g, mt, used)) {
+      mt[to] = v;
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 std::vector<int> Kuhn(int n, int k, const std::vector<std::vector<int>>& g) {
-    std::vector<int> mt(k, -1);
-    std::vector<bool> used(n);
+  std::vector<int> mt(k, -1);
+  std::vector<bool> used(n);
 
-    for (int v = 0; v < n; ++v) {
-        used.assign(n, false);
-        Dfs(v, g, mt, used);
-    }
+  for (int v = 0; v < n; ++v) {
+    used.assign(n, false);
+    Dfs(v, g, mt, used);
+  }
 
-    return mt;
+  return mt;
 }
 
 }  // namespace graph
--- tests/kuhn_test.cpp
+++ tests/kuhn_test.cpp
@@ -1,31 +1,33 @@
 #include "kuhn.hpp"
+
 #include "gtest/gtest.h"
+
 #include <vector>
 
 using namespace graph;
 
 TEST(KuhnTest, SimpleGraph) {
-    std::vector<std::vector<int>> g(3);
-    g[0] = {0, 1};
-    g[1] = {0, 2};
-    g[2] = {1};
+  std::vector<std::vector<int>> g(3);
+  g[0] = {0, 1};
+  g[1] = {0, 2};
+  g[2] = {1};
 
-    std::vector<int> mt = Kuhn(3, 3, g);
+  std::vector<int> mt = Kuhn(3, 3, g);
 
-    int size = 0;
-    for (int i = 0; i < 3; ++i) {
-        if (mt[i] != -1) size++;
-    }
-    EXPECT_EQ(size, 3);
+  int size = 0;
+  for (int i = 0; i < 3; ++i) {
+    if (mt[i] != -1) ++size;
+  }
+  EXPECT_EQ(size, 3);
 }
 
 TEST(KuhnTest, EmptyGraph) {
-    std::vector<std::vector<int>> g(3);
-    std::vector<int> mt = Kuhn(3, 3, g);
+  std::vector<std::vector<int>> g(3);
+  std::vector<int> mt = Kuhn(3, 3, g);
 
-    int size = 0;
-    for (int i = 0; i < 3; ++i) {
-        if (mt[i] != -1) size++;
-    }
-    EXPECT_EQ(size, 0);
+  int size = 0;
+  for (int i = 0; i < 3; ++i) {
+    if (mt[i] != -1) ++size;
+  }
+  EXPECT_EQ(size, 0);
 }
