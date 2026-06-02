7bce97645c6b33bcf40098c80f0a6351f14a7ac8
ireanarch <ireanarch@example.com>
2026-06-02 18:22:23 +0300

    Add Kuhn algorithm implementation and tests

---
+++ include/kuhn.hpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_KUHN_HPP_
+#define INCLUDE_KUHN_HPP_
+
+#include <vector>
+
+namespace graph {
+
+std::vector<int> Kuhn(int n, int k, const std::vector<std::vector<int>>& g);
+
+}  // namespace graph
+
+#endif  // INCLUDE_KUHN_HPP_
---
+++ methods/kuhn_method.cpp
@@ -0,0 +1,35 @@
+#include "kuhn.hpp"
+#include <vector>
+
+namespace graph {
+
+bool Dfs(int v,
+         const std::vector<std::vector<int>>& g,
+         std::vector<int>& mt,
+         std::vector<bool>& used) {
+    if (used[v]) return false;
+    used[v] = true;
+
+    for (size_t i = 0; i < g[v].size(); ++i) {
+        int to = g[v][i];
+        if (mt[to] == -1 || Dfs(mt[to], g, mt, used)) {
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<int> Kuhn(int n, int k, const std::vector<std::vector<int>>& g) {
+    std::vector<int> mt(k, -1);
+    std::vector<bool> used(n);
+
+    for (int v = 0; v < n; ++v) {
+        used.assign(n, false);
+        Dfs(v, g, mt, used);
+    }
+
+    return mt;
+}
+
+}  // namespace graph
