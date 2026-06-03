2f189c8465c90b79fdaf43f4b3ce6b444dca6bed
ge-0rg <9169851346g@gmail.com>
2026-06-03 18:59:41 +0300

    Update kruskal_method.cpp

--- methods/kruskal_method.cpp
+++ methods/kruskal_method.cpp
@@ -11,10 +11,16 @@ using graph::WeightedGraph;
 namespace graph {
 
 /**
- * @brief Метод алгоритма Крускала.
- * @return Функция возвращает 0 в случае успеха
- * и отрицательное число в случае ошибки.
- */
+
+* @brief Метод алгоритма Крускала.
+*
+* @param input Входные данные в формате JSON.
+* @param output Выходные данные в формате JSON.
+*
+* @return Функция возвращает 0 в случае успеха
+* и отрицательное число в случае ошибки.
+  */
+
 int KruskalMethod(const nlohmann::json& input,
     nlohmann::json* output) {
   std::string graphType = input.at("graph_type");
