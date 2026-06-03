06ded9cc5a01bcac118aa63cccb918c9139e5ade
ge-0rg <9169851346g@gmail.com>
2026-06-03 18:50:18 +0300

    Update main.cpp

--- methods/main.cpp
+++ methods/main.cpp
@@ -29,13 +29,16 @@ int main(int argc, char* argv[]) {
     svr.stop();
   });
   svr.Post("/Kruskal", [&](const httplib::Request& req,
-                            httplib::Response& res) {
-    nlohmann::json input = nlohmann::json::parse(req.body);
-    nlohmann::json output;
-    if (graph::KruskalMethod(input, &output) < 0)
-      res.status = 400;
-    res.set_content(output.dump(), "application/json");
-  });
+httplib::Response& res) {
+nlohmann::json input =
+nlohmann::json::parse(req.body);
+nlohmann::json output;
+
+if (graph::KruskalMethod(input, &output) < 0)
+  res.status = 400;
+
+res.set_content(output.dump(),
+                "application/json");
 
   // Эта функция запускает сервер на указанном порту. Программа не завершится
   // до тех пор, пока сервер не будет остановлен.
